dd239265df4dbdf5353ed7b14607d47892fb71a7
Theresa Wilkin <theresawilkin@yahoo.com>
2017-01-05 16:04:19 -0500

    finished scratch array

--- array3.js
+++ array3.js
@@ -9,16 +9,17 @@ var Array = function() {
 Array.RATIO_SIZE = 3;
 
 Array.prototype.resize = function() {
-  // if (this.ptr >= this.length)
   var oldPtr = this.ptr;
   this.ptr = memory.allocate((this.length + 1) * Array.RATIO_SIZE);
+  if(this.ptr === null) {
+    throw new Error("Not enough memory");
+  }
   this.capacity = (this.length + 1) * Array.RATIO_SIZE;
   memory.copy(this.ptr, oldPtr, this.length);
   memory.free(oldPtr);
 }
 
 Array.prototype.push = function(value) {
-  //error?
   if(this.length + 1 > this.capacity) {
     this.resize();
   }
@@ -27,11 +28,54 @@ Array.prototype.push = function(value) {
 }
 
 Array.prototype.get = function(index) {
+  if(this.ptr + index < 0 || index >= this.length) {
+    throw new Error ("Invalid index");
+  }
   return memory.get(this.ptr + index);
 }
 
+Array.prototype.pop = function() {
+  if (this.length == 0) {
+        throw new Error('Array already empty');
+  }
+  var value = memory.get(this.ptr + this.length - 1);
+  this.length --;
+  return value;
+}
 
+Array.prototype.insert = function(index, value) {
+  if(this.ptr + index < 0 || index >= this.length) {
+    throw new Error ("Invalid index");
+  }
+  if(this.length + 1 > this.capacity) {
+    this.resize();
+  }
+  memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
+  memory.set(this.ptr + index, value);
+  this.length ++;
+}
+
+Array.prototype.remove = function(index) {
+  memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1);
+  this.length --;
+}
+
+Array.prototype.shift = function() {
+  var value = memory.get(0)
+  memory.copy(this.ptr, this.ptr + 1, this.length - 1);
+  this.length--;
+  return value;
+}
+
+Array.prototype.unshift = function(value) {
+  memory.copy(this.ptr + 1, this.ptr, this.length);
+  memory.set(this.ptr, value);
+  this.length++;
+}
 var testArr = new Array;
 testArr.push(7);
-console.log(testArr.ptr)
+console.log(testArr.get(0));
+testArr.insert(0, 6);
+console.log(testArr.get(0), testArr.get(1));
+testArr.delete(0);
 console.log(testArr.get(0));
---
+++ arrayQuestions.js
@@ -0,0 +1,21 @@
+// Interview questions
+// Use real arrays; make sure run in O(n)
+
+// 1) Imagine you have an array of numbers. Write an 
+// algorithm to remove all numbers less than five from the array.
+// You shouldn't use the .filter method here; try to write the algorithm from scratch. (Edit in place)
+// 
+// 
+// 2) Imagine you have two arrays which have already been sorted. 
+// Write an algorithm to merge the two arrays into a single array, 
+// which should also be sorted. For example, if your input arrays 
+// were [1, 3, 6, 8, 11] and [2, 3, 5, 8, 9, 10], your output array 
+// should be [1, 2, 3, 3, 5, 6, 8, 8, 9, 10, 11].
+// 
+// 
+// 3) Given an array of numbers, write an algorithm to find out 
+// the products of every number, except the one at that index. For 
+// example, if the input was [1, 3, 9, 4], the output should be 
+// [108, 36, 12, 27] (i.e. [3*9*4, 1*9*4, 1*3*4, 1*3*9]).
+// 
+// 
